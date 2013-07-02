bcec06b1729563dc0b1228e28ba44000367e01eb
Pierre <pierre@exygy.com>
2013-07-02 12:52:59 -0700

    removing build folders

--- mobile/ios/build/barcode.build/Release-iphonesimulator/barcode.build/Objects-normal/i386/TiBarcodeModuleAssets.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/pierreexygy/Desktop/titanium_modules-master/barcode/mobile/ios/Classes/TiBarcodeModuleAssets.m \
-  /Users/pierreexygy/Desktop/titanium_modules-master/barcode/mobile/ios/Classes/TiBarcodeModuleAssets.h
